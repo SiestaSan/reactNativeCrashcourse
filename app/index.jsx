689bb332974682fb531497d3a5e3e297901dd072
SiestaSan <darkflamepasta@gmail.com>
2024-04-12 15:08:26 +0100

    Created setup for app and made a small display Modified files so now app will run on android/ios using expo go

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aora!</Text>
       <StatusBar style="auto" />
+      <Link href = '/profile' style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
   );
 }
@@ -18,3 +20,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
